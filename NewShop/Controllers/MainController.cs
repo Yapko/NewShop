38ca951f33e17386fb4fdcbb4042c97026eb2b3b
Pohlod Slavik <pohlod.slavik@gmail.com>
2012-11-04 23:35:51 +0200

    try commit last changes

---
+++ NewShop/Controllers/MainController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Shop.Views;
+
+namespace Shop.Controllers
+{
+    /// <summary>
+    /// Controller class of our program
+    /// </summary>
+    public class MainController
+    {
+        /// <summary>
+        /// mvc view for this controller
+        /// </summary>
+        private MainWnd view;
+
+        /// <summary>
+        /// mvc model for this controller
+        /// </summary>
+        // private Model model;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainController"/> class.
+        /// </summary>
+        public MainController()
+        {
+            view = new MainWnd(this);
+        //    model = null;
+        }
+       
+        /// <summary>
+        /// Run controller
+        /// </summary>
+        public void Run()
+        {
+            Application.EnableVisualStyles();
+            // Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(view);
+        }
+
+        /// <summary>
+        /// Show message to user
+        /// </summary>
+        public void ShowMessage()
+        {
+            view.ShowMessage("Hello Yapko)!");
+        }
+    }
+}
---
+++ NewShop/Views/MainWnd.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Shop.Controllers;
+
+namespace Shop.Views
+{
+    /// <summary>
+    /// Main form
+    /// </summary>
+    public partial class MainWnd : Form
+    {
+        /// <summary>
+        /// Controll class of this view
+        /// </summary>
+        private MainController control;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWnd"/> class.
+        /// </summary>
+        /// <param name="newControl">class witch controls this form</param>
+        public MainWnd(MainController newControl)
+        {
+            InitializeComponent();
+            control = newControl;
+        }
+
+        /// <summary>
+        /// Shows message to user via messagebox
+        /// </summary>
+        /// <param name="message">message wich want to show</param>
+        public void ShowMessage(string message)
+        {
+            MessageBox.Show(message);
+        }
+
+        /// <summary>
+        /// method of closing picture
+        /// </summary>
+        /// <param name="sender">app sender</param>
+        /// <param name="e">app param</param>
+        private void ClosePricure_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// Method which load all data to form
+        /// </summary>
+        /// <param name="sender">app sender</param>
+        /// <param name="e">app param</param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // to create close icon
+            LoadCloseIcon();
+        }
+
+        /// <summary>
+        /// Create close icon in main form
+        /// </summary>
+        private void LoadCloseIcon()
+        {
+            // create picturebox for icon
+            PictureBox closePictureBox = new PictureBox();
+            // set location in right upper corner
+            closePictureBox.Location = new Point(this.Width - 30, 10);
+            // set icon
+            closePictureBox.Image = Properties.Resources.GreyCloseIcon;
+            // set event 
+            closePictureBox.Click += ClosePricure_Click;
+
+            // creating tooltip for close icon
+            ToolTip closePictureBoxToolTip = new ToolTip();
+            closePictureBoxToolTip.ShowAlways = true;           
+            closePictureBoxToolTip.SetToolTip(closePictureBox, "Close");  
+          
+            // add complete picture box to mainform controls
+            this.Controls.Add(closePictureBox);
+        }
+    }
+}
