eccd0603a08895f8d7ce7ed0680127b581bd1d9e
Yarko Ryabyy <jarkorjabyy@gmail.com>
2012-11-12 20:26:17 +0200

    com

---
+++ NewShop/Views/ProductsListControl.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Shop.Views
+{
+    partial class ProductsListControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ProductsList = new System.Windows.Forms.ListView();
+            this.NameCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.DescribeCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ManufacturerCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.PriceCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // ProductsList
+            // 
+            this.ProductsList.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ProductsList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.NameCol,
+            this.DescribeCol,
+            this.ManufacturerCol,
+            this.PriceCol});
+            this.ProductsList.Location = new System.Drawing.Point(3, 3);
+            this.ProductsList.Name = "ProductsList";
+            this.ProductsList.Size = new System.Drawing.Size(571, 423);
+            this.ProductsList.TabIndex = 0;
+            this.ProductsList.UseCompatibleStateImageBehavior = false;
+            this.ProductsList.View = System.Windows.Forms.View.Details;
+            // 
+            // NameCol
+            // 
+            this.NameCol.Text = "Name";
+            // 
+            // DescribeCol
+            // 
+            this.DescribeCol.Text = "Describe";
+            // 
+            // ManufacturerCol
+            // 
+            this.ManufacturerCol.Text = "Manufacturer";
+            // 
+            // PriceCol
+            // 
+            this.PriceCol.Text = "Price";
+            // 
+            // ProductsListControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Controls.Add(this.ProductsList);
+            this.Location = new System.Drawing.Point(10, 10);
+            this.Name = "ProductsListControl";
+            this.Size = new System.Drawing.Size(577, 429);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView ProductsList;
+        private System.Windows.Forms.ColumnHeader NameCol;
+        private System.Windows.Forms.ColumnHeader DescribeCol;
+        private System.Windows.Forms.ColumnHeader ManufacturerCol;
+        private System.Windows.Forms.ColumnHeader PriceCol;
+    }
+}
---
+++ NewShop/Views/ProductsListControl.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using Shop.AppData;
+using Shop.Controllers;
+using Shop.Models;
+
+namespace Shop.Views
+{
+    /// <summary>
+    /// form for products list
+    /// </summary>
+    public partial class ProductsListControl : UserControl
+    {
+        /// <summary>
+        /// Main controller
+        /// </summary>
+        private MainController main;
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoginControl"/> class.
+        /// </summary>
+        /// <param name="a">instance of comtroller</param>
+        /// <param name="prods">list of products to be added to view</param>
+        public ProductsListControl(MainController a,List<Product> prods)
+        {
+            InitializeComponent();
+            main = a;
+
+            foreach (Product p in prods)
+            {
+                ListViewItem lvi = new ListViewItem(p.Name);
+                lvi.SubItems.Add(p.Describe);
+                lvi.SubItems.Add(p.Maufacture);
+                lvi.SubItems.Add(p.Price.ToString() + "$");
+                AddNewProduct(lvi);
+            }
+            
+        }
+
+        /// <summary>
+        /// method to add new product to view
+        /// </summary>
+        /// <param name="lvi">product to be added</param>
+        public void AddNewProduct(ListViewItem lvi)
+        {
+            ProductsList.Items.Add(lvi);
+        }
+
+    }
+}
