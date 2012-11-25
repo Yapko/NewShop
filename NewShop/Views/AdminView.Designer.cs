081d58a5067221f2be707aa629ad9ae7a32ea5b8
lulia <schonebud@gmail.com>
2012-11-25 21:36:12 +0200

    admin form

---
+++ NewShop/Views/AdminView.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Shop.Views
+{
+    partial class AdminView
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.usersListView = new System.Windows.Forms.ListView();
+            this.usernameColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.statusColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.requestsListView = new System.Windows.Forms.ListView();
+            this.columnHeader12 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader22 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader32 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader52 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.changeStatusToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.managerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.supervisorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loggedUserToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.acceptToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rejectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.contextMenuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(18, 19);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(412, 413);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.usersListView);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(404, 387);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Users";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // usersListView
+            // 
+            this.usersListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.usernameColumn,
+            this.statusColumn});
+            this.usersListView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.usersListView.FullRowSelect = true;
+            this.usersListView.GridLines = true;
+            this.usersListView.LabelEdit = true;
+            this.usersListView.Location = new System.Drawing.Point(3, 3);
+            this.usersListView.Name = "usersListView";
+            this.usersListView.Size = new System.Drawing.Size(398, 381);
+            this.usersListView.TabIndex = 0;
+            this.usersListView.UseCompatibleStateImageBehavior = false;
+            this.usersListView.View = System.Windows.Forms.View.Details;
+            this.usersListView.MouseClick += new System.Windows.Forms.MouseEventHandler(this.UsersListView_MouseClick);
+            // 
+            // usernameColumn
+            // 
+            this.usernameColumn.Text = "Username";
+            this.usernameColumn.Width = 180;
+            // 
+            // statusColumn
+            // 
+            this.statusColumn.Text = "Status";
+            this.statusColumn.Width = 214;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.requestsListView);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(404, 387);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Requests";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // requestsListView
+            // 
+            this.requestsListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader12,
+            this.columnHeader22,
+            this.columnHeader32,
+            this.columnHeader52});
+            this.requestsListView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.requestsListView.FullRowSelect = true;
+            this.requestsListView.GridLines = true;
+            this.requestsListView.LabelEdit = true;
+            this.requestsListView.Location = new System.Drawing.Point(3, 3);
+            this.requestsListView.Name = "requestsListView";
+            this.requestsListView.Size = new System.Drawing.Size(398, 381);
+            this.requestsListView.TabIndex = 1;
+            this.requestsListView.UseCompatibleStateImageBehavior = false;
+            this.requestsListView.View = System.Windows.Forms.View.Details;
+            this.requestsListView.MouseClick += new System.Windows.Forms.MouseEventHandler(this.RequestsListView_MouseClick);
+            // 
+            // columnHeader12
+            // 
+            this.columnHeader12.Text = "Username";
+            this.columnHeader12.Width = 93;
+            // 
+            // columnHeader22
+            // 
+            this.columnHeader22.Text = "Email address";
+            this.columnHeader22.Width = 100;
+            // 
+            // columnHeader32
+            // 
+            this.columnHeader32.Text = "First name";
+            this.columnHeader32.Width = 100;
+            // 
+            // columnHeader52
+            // 
+            this.columnHeader52.Text = "Last name";
+            this.columnHeader52.Width = 100;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.changeStatusToolStripMenuItem,
+            this.deleteToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(156, 48);
+            // 
+            // changeStatusToolStripMenuItem
+            // 
+            this.changeStatusToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.managerToolStripMenuItem,
+            this.supervisorToolStripMenuItem,
+            this.loggedUserToolStripMenuItem});
+            this.changeStatusToolStripMenuItem.Name = "changeStatusToolStripMenuItem";
+            this.changeStatusToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.changeStatusToolStripMenuItem.Text = "Change status";
+            // 
+            // managerToolStripMenuItem
+            // 
+            this.managerToolStripMenuItem.Name = "managerToolStripMenuItem";
+            this.managerToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.managerToolStripMenuItem.Text = "Manager";
+            this.managerToolStripMenuItem.Click += new System.EventHandler(this.ManagerToolStripMenuItem_Click);
+            // 
+            // supervisorToolStripMenuItem
+            // 
+            this.supervisorToolStripMenuItem.Name = "supervisorToolStripMenuItem";
+            this.supervisorToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.supervisorToolStripMenuItem.Text = "Supervisor";
+            this.supervisorToolStripMenuItem.Click += new System.EventHandler(this.SupervisorToolStripMenuItem_Click);
+            // 
+            // loggedUserToolStripMenuItem
+            // 
+            this.loggedUserToolStripMenuItem.Name = "loggedUserToolStripMenuItem";
+            this.loggedUserToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.loggedUserToolStripMenuItem.Text = "LoggedUser";
+            this.loggedUserToolStripMenuItem.Click += new System.EventHandler(this.LoggedUserToolStripMenuItem_Click);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.DeleteToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.acceptToolStripMenuItem,
+            this.rejectToolStripMenuItem});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(119, 48);
+            // 
+            // acceptToolStripMenuItem
+            // 
+            this.acceptToolStripMenuItem.Name = "acceptToolStripMenuItem";
+            this.acceptToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.acceptToolStripMenuItem.Text = "Accept";
+            this.acceptToolStripMenuItem.Click += new System.EventHandler(this.AcceptToolStripMenuItem_Click);
+            // 
+            // rejectToolStripMenuItem
+            // 
+            this.rejectToolStripMenuItem.Name = "rejectToolStripMenuItem";
+            this.rejectToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.rejectToolStripMenuItem.Text = "Reject";
+            this.rejectToolStripMenuItem.Click += new System.EventHandler(this.RejectToolStripMenuItem_Click);
+            // 
+            // AdminView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tabControl1);
+            this.Name = "AdminView";
+            this.Size = new System.Drawing.Size(448, 448);
+            this.Load += new System.EventHandler(this.AdminView_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ListView usersListView;
+        private System.Windows.Forms.ColumnHeader usernameColumn;
+        private System.Windows.Forms.ColumnHeader statusColumn;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem changeStatusToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem managerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem supervisorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loggedUserToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem acceptToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rejectToolStripMenuItem;
+        private System.Windows.Forms.ListView requestsListView;
+        private System.Windows.Forms.ColumnHeader columnHeader12;
+        private System.Windows.Forms.ColumnHeader columnHeader22;
+        private System.Windows.Forms.ColumnHeader columnHeader32;
+        private System.Windows.Forms.ColumnHeader columnHeader52;
+    }
+}
---
+++ NewShop/Views/AdminView.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Shop.AppData;
+using Shop.Controllers;
+
+namespace Shop.Views
+{
+    /// <summary>
+    /// admin's view
+    /// </summary>
+    public partial class AdminView : UserControl
+    {
+        /// <summary>
+        /// main controller
+        /// </summary>
+        private MainController control;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AdminView"/> class.
+        /// </summary>
+        /// <param name="main">main control</param>
+        public AdminView(MainController main)
+        {
+            InitializeComponent();
+            control = main;
+        }
+
+        /// <summary>
+        /// Gets users ListView
+        /// </summary>
+        public ListView UsersList
+        {
+            get
+            {
+                return usersListView;
+            }
+        }
+
+        /// <summary>
+        /// Gets requests ListView
+        /// </summary>
+        public ListView ReqsList
+        {
+            get
+            {
+                return requestsListView;
+            }
+        }
+
+        /// <summary>
+        /// Happens when mouse click on users list view
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">event</param>
+        private void UsersListView_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                Point pt = this.usersListView.PointToScreen(e.Location);
+                this.contextMenuStrip1.Show(pt);
+            }
+        }
+
+        /// <summary>
+        /// Happens when mouse clicked on requestsLisiView
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">event</param>
+        private void RequestsListView_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                Point pt = this.requestsListView.PointToScreen(e.Location);
+                this.contextMenuStrip2.Show(pt);
+            }
+        }
+
+        /// <summary>
+        /// load admin view
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">event</param>
+        private void AdminView_Load(object sender, EventArgs e)
+        {
+            control.LoadUsers();
+            control.LoadRequests();
+        }
+
+        /// <summary>
+        /// Changes user's status to Manager
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">event</param>
+        private void ManagerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem x in this.usersListView.Items)
+            {
+                if (x.Selected)
+                {
+                    this.control.ChangeUserStatus("Manager", x.Text);
+                }
+            }
+            //refresh
+            this.control.LoadUsers();
+        }
+
+        /// <summary>
+        /// Changes user's status to Supervisor
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">event</param>
+        private void SupervisorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem x in this.usersListView.Items)
+            {
+                if (x.Selected)
+                {
+                    this.control.ChangeUserStatus("Supervisor", x.Text);
+                }
+            }
+            //refresh
+            this.control.LoadUsers();
+        }
+
+        /// <summary>
+        /// Changes user's status to LoggedUser
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">event</param>
+        private void LoggedUserToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem x in this.usersListView.Items)
+            {
+                if (x.Selected)
+                {
+                    this.control.ChangeUserStatus("LoggedUser", x.Text);
+                }
+            }
+            //refresh
+            this.control.LoadUsers();
+        }
+
+        /// <summary>
+        /// Happens when Accept tool strip menu item is clicked
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">event</param>
+        private void AcceptToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem x in this.requestsListView.Items)
+            {
+                if (x.Selected)
+                {
+                    MessageBox.Show("sdsd");
+                    this.control.AcceptUser(x.Text);
+                }
+            }
+
+            this.control.LoadRequests();
+            this.control.LoadUsers();
+        }
+
+        /// <summary>
+        /// Happens when Reject tool strip menu item is clicked
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">event</param>
+        private void RejectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem x in this.requestsListView.Items)
+            {
+                if (x.Selected)
+                {
+                    this.control.RejectUser(x.Text);
+                }
+            }
+
+            this.control.LoadRequests();
+        }
+
+        /// <summary>
+        /// Happens when Delete tool strip menu item is clicked
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">event</param>
+        private void DeleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem x in this.usersListView.Items)
+            {
+                if (x.Selected)
+                {
+                    control.DeleteUser(x.Text);
+                }
+            }
+
+            this.control.LoadUsers();
+        }
+    }
+}
